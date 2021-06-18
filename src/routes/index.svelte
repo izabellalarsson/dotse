fa740322a650db9a433814f18fe3037efb615ced
izabellalarsson <izzwii.izabella@gmail.com>
2021-06-18 14:34:04 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Card from '../components/card.svelte';
+</script>
+
+<svelte:head>
+  <title>zbllrsn</title>
+</svelte:head>
+
+<Card />
---
+++ tailwind.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4B5663',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
