27c2f266e5c760f12c5c5a2fa65cafc98d9752f4
izabellalarsson <izzwii.izabella@gmail.com>
2022-03-16 11:10:44 +0100

    feat: sveltekit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+<main class="max-w max-h-full mx-auto">
+	<slot />
+	<!-- <Footer /> -->
+</main>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		@apply bg-lightGrey;
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				// lightbrown: '#695752',
+				// grey: '#696969',
+				// brown: '#62514C'
+				// lightGrey: '#FAFAFA',
+				lightGrey: '#FAF1ED',
+				pink: '#E8C1B5',
+				brown: '#785A4D',
+				darkBlue: '#313C3E',
+				sand: '#CCBAA2',
+				grey: '#A8A195',
+				lightPink: '#E3DBD5'
+			}
+		}
+	},
+	plugins: []
+};
